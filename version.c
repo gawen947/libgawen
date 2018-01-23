c039fc5fbf1d5c2418b45967991c9ad5a8bba9bc
David Hauweele <david@hauweele.net>
2018-01-23 18:27:56 +0100

    Update version.

---
+++ version.c
@@ -0,0 +1,43 @@
+/* Copyright (c) 2018, David Hauweele <david@hauweele.net>
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifdef LIBGAWEN_VERSION
+const char libgawen_version[] = LIBGAWEN_VERSION;
+#endif
+
+#ifdef LIBGAWEN_VERSION_MAJOR
+const unsigned int libgawen_version_major = LIBGAWEN_VERSION_MAJOR;
+#endif
+
+#ifdef LIBGAWEN_VERSION_MINOR
+const unsigned int libgawen_version_minor = LIBGAWEN_VERSION_MINOR;
+#endif
+
+#ifdef LIBGAWEN_VERSION_PATCH
+const unsigned int libgawen_version_patch = LIBGAWEN_VERSION_PATCH;
+#endif
+
+#ifdef LIBGAWEN_COMMIT
+const char libgawen_commit[] = LIBGAWEN_COMMIT;
+#endif
